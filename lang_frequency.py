077f198720a369a76b9acb5e43d6bd8247003478
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-04 16:07:25 +0500

    done

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,30 +2,30 @@ import sys, re
 
 filepath = sys.argv[1]
 
-def load_text(filepath):
+def load_raw_text(filepath):
     fo = open(filepath,'r')
-    text = fo.read()
+    raw_text = fo.read()
     fo.close()
-    return(text)
+    return(raw_text)
 
-def get_most_frequent_words(text):
-    voc = re.findall(r'\w+',text)
+def get_most_frequent_words(raw_text):
+    voc = re.findall(r'\w+',raw_text)
     
-    lsWord = {}
+    word_frequence = {}
     for key in voc:
         key = key.lower()
-        if key in lsWord:
-            value = lsWord[key]
-            lsWord[key]=value+1
+        if key in word_frequence:
+            value = word_frequence[key]
+            word_frequence[key]=value+1
         else:
-            lsWord[key]=1
+            word_frequence[key]=1
     
-    sorted_keys = sorted(lsWord, key=lambda x: int(lsWord[x]), reverse=True)
+    sorted_keys = sorted(word_frequence, key=lambda x: int(word_frequence[x]), reverse=True)
     return(sorted_keys)
 
 if __name__ == '__main__':
-    text = load_text(filepath)
-    frequent_words = get_most_frequent_words(text)
+    raw_text = load_raw_text(filepath)
+    frequent_words = get_most_frequent_words(raw_text)
     i = 0
     while i<=9:
         s = str(frequent_words[i])
