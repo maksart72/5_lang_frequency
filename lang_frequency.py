a3839ad901d68838ccd892112fa4b34957392628
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-14 17:17:33 +0500

    Fixed format PEP8

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,33 +1,33 @@
-import sys, re
+import sys
+import re
+import os
 
-filepath = sys.argv[1]
 
 def load_raw_text(filepath):
-    fo = open(filepath,'r')
-    raw_text = fo.read()
-    fo.close()
-    return(raw_text)
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return file_handler
 
 def get_most_frequent_words(raw_text):
-    voc = re.findall(r'\w+',raw_text)
-    
+    voc = re.findall(r'\w+', raw_text)
     word_frequence = {}
     for key in voc:
         key = key.lower()
         if key in word_frequence:
             frequence = word_frequence[key]
-            word_frequence[key]=frequence+1
+            word_frequence[key] = frequence + 1
         else:
-            word_frequence[key]=1
-    
-    sorted_keys = sorted(word_frequence, key=lambda x: int(word_frequence[x]), reverse=True)
-    return(sorted_keys)
+            word_frequence[key] = 1
+    sorted_keys = sorted(word_frequence, key=lambda x: int(word_frequence[x]), reverse = True)
+    return sorted_keys
 
 if __name__ == '__main__':
-    raw_text = load_raw_text(filepath)
-    frequent_words = get_most_frequent_words(raw_text)
+    filename = sys.argv[1]
+    rawtext = load_raw_text(filename)
+    freqwords = get_most_frequent_words(rawtext)
     keyword = 0
-    while keyword<=9:
-        top = str(frequent_words[keyword])
+    while keyword <= 9:
+        top = str(freqwords[keyword])
         print(top)
         keyword = keyword + 1
