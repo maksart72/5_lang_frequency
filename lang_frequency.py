df8dc4efa59a99bc75e86ebe009b0ec217e1df49
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-21 09:56:26 +0500

    Split calculation and print to different functions.

--- lang_frequency.py
+++ lang_frequency.py
@@ -10,12 +10,18 @@ def load_file(filepath):
     with open(filepath, 'r') as file:
         return file.read()
 
-def print_most_frequent_words(raw_text, top):
+def find_most_frequent_words(raw_text, top):
     vocabluary = re.findall(r'\w+', raw_text.lower())
-    for word in Counter(vocabluary).most_common(top):
-        print(word[0])
+    most_frequent = [word for word in Counter(vocabluary).most_common(top)]
+    return most_frequent
+
+def print_results(most_frequent):
+    for word in most_frequent:
+        print(word[0],' - founded ',word[1],' times')
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     how_many_words = 10
-    print_most_frequent_words(load_file(filename), how_many_words)
+    result = find_most_frequent_words(load_file(filename), how_many_words)
+    print_results(result)
+    
