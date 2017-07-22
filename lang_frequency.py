422879a8793cf77522853c5bcaf3fb294cdfa0d9
maksart72 <maksim.artemyev@gmail.com>
2017-07-22 20:27:15 +0500

    Fixed mistakes from 21/07

--- lang_frequency.py
+++ lang_frequency.py
@@ -7,21 +7,24 @@ from collections import Counter
 def load_file(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath, 'r') as file:
+    with open(filepath, 'r') as file: 
         return file.read()
 
 def find_most_frequent_words(raw_text, top):
     vocabluary = re.findall(r'\w+', raw_text.lower())
-    most_frequent = [word for word in Counter(vocabluary).most_common(top)]
+    most_frequent = Counter(vocabluary).most_common(top)
     return most_frequent
 
 def print_results(most_frequent):
     for word in most_frequent:
-        print(word[0],' - founded ',word[1],' times')
+        print(word[0], ' - founded ', word[1], ' times')
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    how_many_words = 10
-    list_most_frequent = find_most_frequent_words(load_file(filename), how_many_words)
-    print_results(list_most_frequent)
+    if not os.path.exists(filename):
+        print("Error! Check your file!")
+    else:
+        how_many_words = 10
+        list_most_frequent = find_most_frequent_words(load_file(filename), how_many_words)
+        print_results(list_most_frequent)
     
